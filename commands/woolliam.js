6eca083ff57903e75daa52ea861d4cc887df759c
Joshua Mollet <joshua.mollet@gmail.com>
2021-09-29 18:58:55 +0200

    rework wooll 2

--- commands/woolliam.js
+++ commands/woolliam.js
@@ -3,7 +3,13 @@ module.exports = {
     name: "woolliam",
     description: "alias Wingliam",
     execute(message){
+        // message.channel.send('J\'peux pas j\'attends New World');
+        // message.channel.send('https://tenor.com/view/new-world-gif-22562955');
         message.channel.send('J\'peux pas j\'attends dans New World');
-        message.channel.send('../img/att.png');
+        // message.chanel.send('J\'peux pas j\'attends dans New World', { files : 'https://i.ibb.co/RywTyyX/att.png'});
+
+        message.channel.send({
+            files: ['https://i.ibb.co/RywTyyX/att.png']
+          })
     }
 }
